b084b2a9ca3b4e9ef76caad2d5640ed030dbcf93
vevina-zw <1269275592@qq.com>
2019-02-14 18:25:31 +0800

    获取微信用户信息、、我的积分

--- pages/my/my.js
+++ pages/my/my.js
@@ -19,23 +19,31 @@ Page({
       { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_order4.png`, name: '已完成', url: '', type: 'sign'  },
     ],
     myLink1:[
-      { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon1.png`,name:'收货地址',url:''},
+      { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon1.png`, name: '收货地址', url:'/pages/address/myAddress/myAddress'},
       { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon2.png`, name: '购物车', url: '/pages/cart/cart' }
     ],
     myLink2: [
-      { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon3.png`, name: '邀请奖励', url: '' },
-      { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon4.png`, name: '我的积分', url: '' },
+      { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon3.png`, name: '邀请奖励', url: '/pages/communityIntro/communityIntro' },
+      { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon4.png`, name: '我的积分', url: '/pages/my/myPoints/myPoints' },
       { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon5.png`, name: '返现', url: '/pages/my/myBalance/myBalance' },
       { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon6.png`, name: '优惠券', url: '/pages/my/myCoupon/myCoupon' },
       { icon: `${app.globalData.baseUrl}/img/WeChat/myCenter_linkIcon7.png`, name: '课程', url: '/pages/my/myCourse/myCourse' },
-    ]
+    ],
+
+    userInfo: null,
+    hasUserInfo: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }
   },
 
   /**
@@ -91,6 +99,18 @@ Page({
     console.log(type)
     wx.navigateTo({
       url: `/packages/trade/index?pageType=order&type=${type}&openId=${this.data.openId}`
+    })
+  },
+
+  getUserInfo: function(){
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
     })
   }
 })
